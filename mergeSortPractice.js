ecb31afe55a748db531f38353ed52b144470cea6
Christina Jasik <cjjasik@gmail.com>
2024-03-25 13:28:13 -0400

    mergeSort application

--- mergeSortPractice.js
+++ mergeSortPractice.js
@@ -3,7 +3,7 @@
  * 
  */
 
-function mergeSort(arrayOne, arrayTwo) {
+function merge(arrayOne, arrayTwo) {
     let sortedArray = [];
 
     while (arrayOne.length && arrayTwo.length) {
@@ -17,5 +17,20 @@ function mergeSort(arrayOne, arrayTwo) {
     return [...sortedArray, ...arrayOne, ...arrayTwo];
 }
 
+function mergeSort(arr) {
+  // quick check for length at its simplest form
+  if (arr.length <= 1){
+    return arr;
+  }
 
-console.log(mergeSort([1, 4, 5], [2, 3, 35, 84]));
+  // finding the middle of the array
+  let mid = Math.floor(arr.length / 2)
+  // calls left array and right array
+  // calling mergeSort breaks left and right down
+  let left = mergeSort(arr.slice(0, mid))
+  let right = mergeSort(arr.slice(mid))
+
+  return merge(left, right)
+}
+
+console.log(mergeSort([3, 5, 8, 5, 99, 1]))
